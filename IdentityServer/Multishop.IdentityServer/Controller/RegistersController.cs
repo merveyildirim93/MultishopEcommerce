88a4a8d9c3c9d5fa554d8222f8fb9b53b3ebaa9a
Merve Yıldırım <merve.yildirimm.93@hotmail.com>
2024-05-07 01:04:54 +0300

    identityserver4 migration işlemleri

---
+++ IdentityServer/Multishop.IdentityServer/Controller/RegistersController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Multishop.IdentityServer.Dtos;
+using Multishop.IdentityServer.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Multishop.IdentityServer.Controller
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserRegister(UserRegisterDto dto)
+        {
+            var value = new ApplicationUser()
+            {
+                UserName = dto.Username,
+                Email = dto.Email,
+                Name = dto.Name,
+                Surname = dto.Surname
+            };
+            var result = await _userManager.CreateAsync(value, dto.Password);
+            if (result.Succeeded)
+            {
+                return Ok("Kullanıcı başarıyla eklendi");
+            }
+            else
+            {
+                return Ok("Kullanıcı eklenirken hata oluştu");
+            }
+
+        }
+
+    }
+}
