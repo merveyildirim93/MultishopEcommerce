0d33a30822d7079b51b20ca39fb6d4280bc5f2e0
Merve Yıldırım <merve.yildirimm.93@hotmail.com>
2024-07-01 00:07:31 +0300

    layout işlemleri

---
+++ Frontend/MultishopEcommerce.WebUI/ViewComponents/UILayoutViewComponents/_NavbarUIComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultishopEcommerce.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _NavbarUIComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
