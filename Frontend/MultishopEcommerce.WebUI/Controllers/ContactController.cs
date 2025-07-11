7147aae396521c5a7cc090e1993a85e8f85d5819
Merve Yıldırım <merve.yildirimm.93@hotmail.com>
2025-07-12 01:55:08 +0300

    add shoppingcard components

---
+++ Frontend/MultishopEcommerce.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultishopEcommerce.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontend/MultishopEcommerce.WebUI/ViewComponents/ShoppingCardViewComponents/_ShoppingCardDiscountCouponComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultishopEcommerce.WebUI.ViewComponents.ShoppingCardViewComponents
+{
+    public class _ShoppingCardDiscountCouponComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
